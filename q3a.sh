2fb02728f99107e5117307f3169290033ae8a2ab
jason <peepaz@gmail.com>
2019-12-07 12:08:50 -0400

    third commit

---
+++ q3a.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install -y gnupg2
+gpg --import labexam.gpg
+read -p "enter a message." msg
+echo "$msg" > message.txt
+gpg --encrypt --recipient 'Nicholas Mendez' message.txt
